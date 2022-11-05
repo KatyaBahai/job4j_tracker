d68a30984860d7e5681b4be3274669f2a8ab73eb
KatyaBahai <katyxa-two@yandex.ru>
2022-11-05 16:52:23 -0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public void info() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.info();
+        Error errorOne = new Error(true, 10, "pending");
+        errorOne.info();
+        Error errorTwo = new Error(true, 1, "undefined");
+        errorTwo.info();
+        Error errorThree = new Error(false, 0, "No error found");
+        errorThree.info();
+    }
+
+}
