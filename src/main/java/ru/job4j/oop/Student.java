45c61d5c25061266f4855c7f01cf8579895b67ce
KatyaBahai <katyxa-two@yandex.ru>
2022-11-06 14:11:55 -0600

    1.3. Вызов метода с аргументами. [#166086 #357995]0

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -4,9 +4,11 @@ public class Student {
     public void music() {
         System.out.println("Tra tra tra");
     }
+
     public void song() {
         System.out.println("I believe I can fly");
     }
+
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
@@ -15,6 +17,5 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
-
     }
 }
