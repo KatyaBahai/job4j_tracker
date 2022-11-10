8cf7b1958d2afd39948c5be16715c249194ed967
KatyaBahai <katyxa-two@yandex.ru>
2022-11-10 15:30:48 -0600

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,17 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return created;
     }
 
     public Item(String name) {
