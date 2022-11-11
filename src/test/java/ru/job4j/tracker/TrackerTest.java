84e5c62c72a87895e331f4c8287adb47e8edcbbe
KatyaBahai <katyxa-two@yandex.ru>
2022-11-11 16:41:45 -0600

    5. Tracker - хранилище [#396]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class TrackerTest {
     @Test
     public void whenTestFindById() {
