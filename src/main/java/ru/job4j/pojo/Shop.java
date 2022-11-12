1a1f65a6480ffae9544914a659d261bce1e83350
KatyaBahai <katyxa-two@yandex.ru>
2022-11-12 15:22:33 -0600

    3.1. Массив с пустыми ячейками [#176470 #360087]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -10,14 +10,12 @@ public class Shop {
     }
 
     public static int indexOfNull(Product[] products) {
-        int indexNull = -1;
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product == null) {
-               indexNull = i;
-                return indexNull;
+                return i;
             }
         }
-        return indexNull;
+        return -1;
     }
 }
