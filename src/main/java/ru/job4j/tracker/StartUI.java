485b574c3e6954c428d99c954beaa7e03eb8c2b7
KatyaBahai <katyxa-two@yandex.ru>
2022-11-16 14:02:34 -0600

    2.6. Реализация класса StartUI. Удаление заявки [#500747 #361213]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -40,7 +40,7 @@ public class StartUI {
                }
             } else if (select == 3) {
                 System.out.println("=== Delete item ===");
-                System.out.println("Enter the id");
+                System.out.print("Enter the id");
                 int id = Integer.parseInt(scanner.nextLine());
                 if (tracker.delete(id)) {
                     System.out.println("Заявка удалена успешно.");
