2c5a09906120ad22646f34c713fc7110fe471ea2
KatyaBahai <katyxa-two@yandex.ru>
2022-11-18 22:12:40 -0600

    8. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+public class ReplaceAction implements UserAction {
+    @Override
+    public String name() {
+        return "Edit item";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Edit item ===");
+        int id = input.askInt("Enter id: ");
+        String name = input.askStr("Enter name: ");
+        Item newItem = new Item(name);
+        if (tracker.replace(id, newItem)) {
+            System.out.println("Заявка изменена успешно.");
+        } else {
+            System.out.println("Ошибка замены заявки.");
+        }
+        return true;
+    }
+}
