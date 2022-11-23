363d82549067dd469132c265791359f3ca7b13e8
KatyaBahai <katyxa-two@yandex.ru>
2022-11-23 17:45:34 -0600

    4. Принципы раннего возврата и охранных выражений [#504875 #363362]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -12,68 +12,19 @@ public class PasswordValidator {
                     "A password should be [8-32] symbols long."
             );
         }
-        if (!hasUpperCase(characters)) {
-            throw new IllegalArgumentException(
-                    "A password should contain at least one uppercase letter."
-            );
-        }
-        if (!hasLowerCase(characters)) {
-            throw new IllegalArgumentException(
-                    "A password should contain at least one lowercase letter."
-            );
-        }
-        if (!hasSpecialCharacter(characters)) {
-            throw new IllegalArgumentException(
-                    "A password should contain at least one special character."
-            );
-        }
-        if (!hasDigit(characters)) {
-            throw new IllegalArgumentException(
-                    "A password should contain at least one digit."
-            );
-        }
         if (hasSubstring(password)) {
             throw new IllegalArgumentException(
                     "A password shouldn't contain substrings: qwerty, 12345, password, admin, user."
             );
         }
+        if (!hasCorrectChars(characters)) {
+            throw new IllegalArgumentException(
+                    "You must have a least one uppercase letter, " +
+                            "one lowercase letter, one special character " +
+                            "and one digit."
+            );
+        }
         return password;
-    }
-
-    public static boolean hasUpperCase(char[] characters) {
-        for (char character : characters) {
-            if (Character.isUpperCase(character)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean hasLowerCase(char[] characters) {
-        for (char character : characters) {
-            if (Character.isLowerCase(character)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean hasSpecialCharacter(char[] characters) {
-        for (char character : characters) {
-            if (!Character.isLetterOrDigit(character)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean hasDigit(char[] characters) {
-        for (char character : characters) {
-            if (Character.isDigit(character)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     public static boolean hasSubstring(String password) {
@@ -85,4 +36,49 @@ public class PasswordValidator {
         }  return false;
     }
 
+    public static boolean hasCorrectChars(char[] characters) {
+        int hasUpperCase = 0;
+        int hasLowerCase = 0;
+        int hasDigit = 0;
+        int hasSpecialChar = 0;
+
+        for (char character : characters) {
+            if (Character.isUpperCase(character)) {
+                hasUpperCase++;
+            }
+            if (Character.isLowerCase(character)) {
+                hasLowerCase++;
+            }
+            if (Character.isDigit(character)) {
+                hasDigit++;
+            }
+            if (!Character.isLetterOrDigit(character)) {
+                hasSpecialChar++;
+            }
+            if (hasUpperCase > 0 && hasLowerCase > 0 && hasDigit > 0 && hasSpecialChar >0) {
+                return true;
+            }
+        }
+        if (hasUpperCase == 0) {
+            throw new IllegalArgumentException(
+                    "A password should contain at least one uppercase letter."
+            );
+        }
+        if (hasLowerCase == 0) {
+            throw new IllegalArgumentException(
+                    "A password should contain at least one lowercase letter."
+            );
+        }
+        if (hasDigit == 0) {
+            throw new IllegalArgumentException(
+                    "A password should contain at least one digit."
+            );
+        }
+        if (hasSpecialChar == 0) {
+            throw new IllegalArgumentException(
+                    "A password should contain at least one special character."
+            );
+        }
+        return false;
+    }
 }
