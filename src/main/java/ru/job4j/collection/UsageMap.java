aa20527c66e13bc49ee2a88a93da4d6e73759898
KatyaBahai <katyxa-two@yandex.ru>
2022-12-15 22:46:04 +0600

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,7 +7,7 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("katyxa-two@yandex.ru", "Katya");
-        for (Map.Entry<String, String> entry : map.entrySet()){
+        for (Map.Entry<String, String> entry : map.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
             System.out.println("The email is " + key + " and the name is " + value);
